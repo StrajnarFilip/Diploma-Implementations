b3ba2f2eaa7e616179e0bf0e810a14d87d00d767
Filip Strajnar <filip.strajnar@gmail.com>
2022-08-23 13:51:08 +0200

    corrected id shape

--- phoenix/blog/lib/blog/post/create.ex
+++ phoenix/blog/lib/blog/post/create.ex
@@ -1,7 +1,7 @@
 defmodule Blog.Post.Create do
   def new_post(title) do
     case %Schema.Post{title: title} |> Blog.Repo.insert(returning: true) do
-      {:ok, %Schema.Post{idpost: new_id}} -> new_id
+      {:ok, %Schema.Post{idpost: new_id}} -> %{id: new_id}
       _ -> "Failed to create new post."
     end
   end
