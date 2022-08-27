77ce5770d8a753859d455b2378b13e9a971a6dc1
Filip Strajnar <filip.strajnar@gmail.com>
2022-08-27 21:45:13 +0200

    Single connection open

--- spring/blog/src/main/java/development/blog/data/Database.java
+++ spring/blog/src/main/java/development/blog/data/Database.java
@@ -9,18 +9,20 @@ import java.util.Properties;
 public class Database {
     final String url = "jdbc:postgresql://localhost:5433/postgres";
     Properties props;
+    Connection connection;
 
     public Database() {
         props = new Properties();
         props.setProperty("user", "postgres");
         props.setProperty("password", "f5da15f2addf6857266afd80d19bd20da241f8bf334af04f");
+        try {
+            this.connection = DriverManager.getConnection(url, props);
+        } catch (Exception e) {
+            this.connection = null;
+        }
     }
 
     public Connection getConnection() {
-        try {
-            return DriverManager.getConnection(url, props);
-        } catch (Exception e) {
-            return null;
-        }
+        return connection;
     }
 }
