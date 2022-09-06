9b5dab7b80e1227f4f0be7e89b6aba8cccec0a5c
Filip Strajnar <filip.strajnar@gmail.com>
2022-09-06 18:04:18 +0200

    waitress flask

--- flask/blog/blog/__main__.py
+++ flask/blog/blog/__main__.py
@@ -1,5 +1,6 @@
 from flask_cors import CORS
 from flask import Flask
+from waitress import serve
 
 from comment.comment import comment_routes
 from segment.segment import segment_routes
@@ -14,4 +15,6 @@ segment_routes(app)
 post_routes(app)
 user_routes(app)
 
-app.run(port=7000)
+serve(app, host='127.0.0.1', port=7000,
+threads=8, connection_limit=200,backlog=50_000,channel_timeout=500,
+asyncore_use_poll=True)
